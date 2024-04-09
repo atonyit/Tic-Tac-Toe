9530d0d1c5004fe9285d054d2063f9942fedfeb0
Alan <alanitoyah96@gmail.com>
2024-04-09 16:38:05 -0500

    Updating logic for the javascript portion

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ const strike = document.getElementById('strike');
 const gameOverArea = document.getElementById('game-over-area');
 const gameOverText = document.getElementById('game-over-text');
 const playAgain = document.getElementById('play-again');
+
+const gameOverSound = new Audio(".vscode/sounds/game_over.wav");
+const clickSound = new Audio(".vscode/sounds/click.wav");
+
+tiles.forEach((tile) => tile.addEventListener("click", tileClick));
+
+function tileClick(event){
+    if(gameOverArea.classList.contains('visible')){
+        return;
+    }
+
+    const tile = event.target;
+    const tileNumber = tile.dataset.index;
+    if(tile.innerText != ""){
+        return;
+    }
+
+    if(turn === PLAYER_X){
+        tile.innerText = PLAYER_X;
+        boardState[tileNumber - 1]
+    }
+}
+
